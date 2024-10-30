c27bac315437a4c97f813071b85625672658b1ab
John Michael Hinacay <91406648+JMHinacay@users.noreply.github.com>
2024-10-30 14:36:34 +0800

    initialize app and electron

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div style={{ height: "100vh" }}>
+      <iframe
+        style={{ borderWidth: 0 }}
+        title="test"
+        height={"100%"}
+        width="100%"
+        src="https://mobile.dentistfind.com"
+      ></iframe>
+    </div>
+  );
+}
+
+export default App;
